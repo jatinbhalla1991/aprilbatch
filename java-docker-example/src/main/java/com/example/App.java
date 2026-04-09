a8437123e1da4d82fb4265b40ee64bdae66728ef
tarunsepta1995 <tarunsepta1995@gmail.com>
2026-04-09 11:33:58 +0530

    compose test

--- java-docker-example/src/main/java/com/example/App.java
+++ java-docker-example/src/main/java/com/example/App.java
@@ -10,6 +10,9 @@ public class App {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(App.class);
         app.setDefaultProperties(Collections.singletonMap("server.port", "8081"));
+        app.setDefaultProperties(Collections.singletonMap("spring.datasource.url", "jdbc:mysql://db:3306/mydatabase"));
+        app.setDefaultProperties(Collections.singletonMap("spring.datasource.username", "root"));
+        app.setDefaultProperties(Collections.singletonMap("spring.datasource.password", "rootpassword"));
         app.run(args);
     }
 }
